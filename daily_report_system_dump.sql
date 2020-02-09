54d1458122a98b0483d80aa55120949f3ea4cfd7
parma <techacademy114514@gmail.com>
2020-02-10 04:26:48 +0900

    deploy

--- daily_report_system_dump.sql
+++ daily_report_system_dump.sql
@@ -14,6 +14,68 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_flag` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `delete_flag` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_3um79qgwg340lpaw7phtwudtc` (`code`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,1,'5413','2020-02-10 03:14:08',0,'あやしいひかり','58D7DFA10A8F9FA66784814C3C2C02D80F72E77FF632FBB856DF220DB757AD45','2020-02-10 03:14:08');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` longtext NOT NULL,
+  `created_at` datetime NOT NULL,
+  `end` varchar(255) NOT NULL,
+  `report_date` date NOT NULL,
+  `start` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK7m58c0h3phjcm6bco7aqikfen` (`employee_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reports`
+--
+
+LOCK TABLES `reports` WRITE;
+/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +86,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-09 16:29:31
+-- Dump completed on 2020-02-10  3:26:33
